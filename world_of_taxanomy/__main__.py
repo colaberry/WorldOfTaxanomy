d321de87df05f1e10e3430b9178ef4c8f25513f3
ramdhanyk <ram@colaberry.com>
2026-04-12 08:46:45 -0400

    feat: ingest ISCO-08 (619 nodes, TDD green)

--- world_of_taxanomy/__main__.py
+++ world_of_taxanomy/__main__.py
@@ -184,6 +184,12 @@ def cmd_ingest(args):
                 n = await ingest_soc_2018(conn)
                 print(f"  {n} nodes")
 
+            if target in ("isco_08", "all"):
+                from world_of_taxanomy.ingest.isco_08 import ingest_isco_08
+                print("\n-- ISCO-08 --")
+                n = await ingest_isco_08(conn)
+                print(f"  {n} nodes")
+
         await close_pool()
 
     _run(_ingest())
@@ -392,7 +398,7 @@ def build_parser() -> argparse.ArgumentParser:
     p_ingest = sub.add_parser("ingest", help="Ingest classification data")
     p_ingest.add_argument(
         "target",
-        choices=["naics", "isic", "nic", "nace", "sic", "anzsic", "jsic", "wz", "onace", "noga", "crosswalk", "iso3166_1", "iso3166_2", "crosswalk_iso3166", "un_m49", "crosswalk_un_m49_iso3166", "hs2022", "crosswalk_hs_isic", "cpc_v21", "crosswalk_cpc_isic", "crosswalk_cpc_hs", "unspsc_v24", "soc_2018", "all"],
+        choices=["naics", "isic", "nic", "nace", "sic", "anzsic", "jsic", "wz", "onace", "noga", "crosswalk", "iso3166_1", "iso3166_2", "crosswalk_iso3166", "un_m49", "crosswalk_un_m49_iso3166", "hs2022", "crosswalk_hs_isic", "cpc_v21", "crosswalk_cpc_isic", "crosswalk_cpc_hs", "unspsc_v24", "soc_2018", "isco_08", "all"],
         help="What to ingest",
     )
 
